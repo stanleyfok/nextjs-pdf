d788314200970b1a70716958ee3f6287d04de6c6
Stanley Fok <cfok@ebay.com>
2018-11-06 11:42:20 +0800

    ignore express

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,15 @@ class IndexPage extends React.Component {
     if (isServer && exportPDF) {
       const buffer = await pdfHelper.componentToPDFBuffer(<Article/>)
 
-      res.setHeader(
-        'Content-disposition',
-        'attachment; filename="article.pdf',
-        );
-      res.type('pdf');
-      res.send(buffer);
+      // with this header, your browser will prompt you to download the file
+      // without this header, your browse will open the pdf directly
+      res.setHeader('Content-disposition', 'attachment; filename="article.pdf');
+      
+      // set content type
+      res.setHeader('Content-Type', 'application/pdf');
+
+      // output the pdf buffer. once res.end is triggered, it won't trigger the render method
+      res.end(buffer);
     }
 
     return {};
